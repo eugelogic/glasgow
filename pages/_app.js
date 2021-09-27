7df36460a83c757de4d007a97e78a791d93b17d5
Eugene Molari <eugelogic@gmail.com>
2021-09-27 14:05:25 +0100

    Remove redundant content

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+const Tekton = ({ Component, pageProps }) => {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default Tekton
