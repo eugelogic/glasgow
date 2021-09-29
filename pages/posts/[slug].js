73583c33335c5bc75ff997160f267465c1005070
Eugene Molari <eugelogic@gmail.com>
2021-09-29 20:41:24 +0100

    Add Like functionality to single post template

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,4 +1,6 @@
 import { useRouter } from 'next/router'
+import { useState } from 'react'
+import { BiLike } from "react-icons/bi";
 import Image from 'next/image'
 import {
     sanityClient,
@@ -20,6 +22,7 @@ const postQuery = `*[ _type == 'post' && slug.current == $slug ][0]{
     },
     publishedAt,
     body,
+    likes
 }`
 
 export const getStaticPaths = async () => {
@@ -47,16 +50,35 @@ export const getStaticProps = async ({ params }) => {
 }
 
 const Post = ({ data }) => {
+
+    const [likes, setLikes] = useState(data?.post?.likes)
+    const handleLike = async () => {
+        const res = await fetch('/api/handle-like', {
+            method: 'POST',
+            body: JSON.stringify({ _id: post._id})
+        }).catch((error) => console.log(error))
+
+        const data = await res.json()
+        setLikes(data.likes)
+    }
+
     const router = useRouter()
     if (router.isFallback) {
         return <div><p>Loading ...</p></div>
     }
+
     const { post } = data
     // console.log(post)
+    console.log(likes)
     return (
         <article>
             <h1>{post.title}</h1>
             <Image src={urlFor(post.mainImage).url()} alt={post.mainImage.alt} width={600} height={400} />
+            <div>
+                <button onClick={handleLike}>
+                    {likes} <BiLike />
+                </button>
+            </div>
             <h3>Category:</h3>
             <ul>{post.categories.map(cat => (
                 <li key={cat._id}>
