badc09560177a7447e83ed928aa30c18633fcb16
Eugene Molari <eugelogic@gmail.com>
2021-10-07 11:43:25 +0100

    Add homepage link to site title in the Header component

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,14 @@
+import Link from 'next/link'
+
 const Header = ({ siteSettings }) => {
 
     return(
         <header className="header">
-            <h1 className="site-title">{siteSettings.siteName}</h1>
+            <Link href="/">
+                <a aria-label="Go Home">
+                    <h1 className="site-title">{siteSettings.siteName}</h1>
+                </a>
+            </Link>
         </header>
     )
 }
