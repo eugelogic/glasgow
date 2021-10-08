f19d0eb2ef7995aa5eb787b31becba4f7704d053
Eugene Molari <eugelogic@gmail.com>
2021-10-08 10:43:21 +0100

    Add link to source code

--- components/Footer.js
+++ components/Footer.js
@@ -2,6 +2,7 @@ const Footer = ({ siteSettings }) => {
 
     return (
         <footer className="footer">
+            <a href="https://github.com/eugelogic/gothenburg" target="_blank" rel="noreferrer noopener">View Source Code</a>
             <p>&copy; {new Date().getFullYear()} {siteSettings.siteName}</p>
         </footer>
     )
