4bcb50ac3dc1deed5890e4e92f843753a26f9946
Eugene Molari <eugelogic@gmail.com>
2021-11-15 14:53:52 +0000

    Link Google Font the proper way

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,3 @@
-/* eslint(@next/next/no-page-custom-font) */
 import { useRouter } from 'next/router'
 import Head from 'next/head'
 import Footer from './Footer'
@@ -14,9 +13,6 @@ const Layout = ({ siteSettings, post, children }) => {
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
                 <title>{router.pathname === '/' ? `${siteSettings?.shortDescription} | ${siteSettings?.siteName}` : `${post?.title} | ${siteSettings?.siteName}`}</title>
                 <meta name="description" content={siteSettings?.description} />
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-                <link href="https://fonts.googleapis.com/css2?family=Baumans&family=Montserrat:wght@400;600&display=swap" rel="stylesheet" />
             </Head>
             <div className="container">
                 <Header siteSettings={siteSettings} />
